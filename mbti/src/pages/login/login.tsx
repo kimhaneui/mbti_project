5261c3e80da4432c44a7bb3a422427ab8639e542
kimhaneui <gksmlskfk2@gmail.com>
2023-07-07 21:06:59 +0900

    label 수정

--- mbti/src/pages/login/login.tsx
+++ mbti/src/pages/login/login.tsx
@@ -17,23 +17,23 @@ export function LoginPage() {
         <div>이미지</div>
         <div className="form-group">
           <label htmlFor="username">이메일</label>
-          <input type="text" id="username" placeholder="Enter your username" />
+          <input type="text" id="username" placeholder="이메일 입력" />
         </div>
         <div className="form-group">
           <label htmlFor="password">이름</label>
-          <input type="password" id="password" placeholder="Enter your password" />
+          <input type="password" id="password" placeholder="이름 입력" />
         </div>
         <div className="form-group">
           <label htmlFor="password">닉네임</label>
-          <input type="password" id="password" placeholder="Enter your password" />
+          <input type="password" id="password" placeholder="닉네임 입력" />
         </div>
         <div className="form-group">
           <label htmlFor="password">MBTI</label>
-          <input type="password" id="password" placeholder="Enter your password" />
+          <input type="password" id="password" placeholder="예시 - INFJ" />
         </div>
         <div className="form-group">
           <label htmlFor="password">한줄소개</label>
-          <input type="password" id="password" placeholder="Enter your password" />
+          <input type="password" id="password" placeholder="한줄소개 입력" />
         </div>
         <div className="joinBtn">
           <Button label="COME ON" backgroundColor="pink" onClick={() => navigate('/home')}></Button>
